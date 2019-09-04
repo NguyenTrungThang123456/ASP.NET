c38b0270423beafd06b0fc3d8f4a420c371d355e
NguyenTrungThang123456 <thangnt.nde18074@vtc.edu.vn>
2019-09-05 00:21:35 +0700

    First ASP.NET Web App

---
+++ Data/MovieAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace MovieApp.Models
+{
+    public class MovieAppContext : DbContext
+    {
+        public MovieAppContext (DbContextOptions<MovieAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; }
+    }
+}
